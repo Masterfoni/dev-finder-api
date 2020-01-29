20e762843748397d7ceeff6c74bac0ca1d247ce3
Masterfoni <joaovitor8891879@gmail.com>
2020-01-28 22:22:55 -0300

    Add socketio and create event for connections

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 require('dotenv').config();
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-5ytfa.mongodb.net/devfinder?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
@@ -16,4 +22,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
